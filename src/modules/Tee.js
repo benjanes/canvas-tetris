230c3fe585fa068aff157b123da6c715fa1ffa1c
Ben Janes <brjanes@gmail.com>
2016-09-20 09:26:20 -0400

    Add rotation fns for Tee shape

--- src/modules/Tee.js
+++ src/modules/Tee.js
@@ -16,7 +16,54 @@ export default class Tee extends Piece {
     ];
   }
 
-  rotate() {
-    return;
+  getChangeFns() {
+    let x, y, changeInX, changeInY;
+
+    x = this.cells[1].x;
+    y = this.cells[1].y;
+
+    if (this.cells[0].y > this.cells[3].y && this.cells[0].x < this.cells[3].x) {
+      // upside-down T
+      changeInX = (i) => {
+        if (i === 3) return x + 1;
+        return x;
+      };
+      changeInY = (i) => {
+        if (i !== 3) return y - 1 + i;
+        return y;
+      };
+    } else if (this.cells[0].y < this.cells[3].y && this.cells[0].x < this.cells[3].x) {
+      // sideways pointing right
+      changeInX = (i) => {
+        if (i !== 3) return x + 1 - i;
+        return x;
+      };
+      changeInY = (i) => {
+        if (i === 3) return y + 1;
+        return y;
+      };
+    } else if (this.cells[0].y < this.cells[3].y && this.cells[0].x > this.cells[3].x) {
+      // T
+      changeInX = (i) => {
+        if (i === 3) return x - 1;
+        return x;
+      }
+      changeInY = (i) => {
+        if (i !== 3) return y + 1 - i;
+        return y;
+      }
+    } else if (this.cells[0].y > this.cells[3].y && this.cells[0].x > this.cells[3].x) {
+      // sideways pointing left
+      changeInX = (i) => {
+        if (i !== 3) return x - 1 + i;
+        return x;
+      }
+      changeInY = (i) => {
+        if (i === 3) return y - 1;
+        return y;
+      }
+    }
+
+    return { changeInX, changeInY };
   }
 }
