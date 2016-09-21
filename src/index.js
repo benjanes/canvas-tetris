07d6b6b485296d9b17f907096f518d6bddd3aa0a
Ben Janes <brjanes@gmail.com>
2016-09-21 09:41:08 -0400

    Clean up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,3 @@ import Board from './modules/Board';
 const tetrisGame = new Board(20, 20, document.getElementById('tetris_board'));
 document.addEventListener('keydown', tetrisGame.handleKeydown.bind(tetrisGame));
 tetrisGame.drawBoard();
-
-// const square = new Square(20);
-// console.log(square);
