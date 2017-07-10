2f876f189c342df9d0a1d72db09fd351b7f771b6
Ben Janes <brjanes@gmail.com>
2017-07-10 19:12:56 -0400

    Add placeholder level bg styles

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,32 @@
 export default {
 	1: {
-		backgroundColor: '#ff0000',
-		color: '#000'
+		backgroundColor: 'rgba(0,0,0,0.7)'
+	},
+	2: {
+		backgroundColor: 'rgba(0,0,0,0.5)'
+	},
+	3: {
+		backgroundColor: 'rgba(0,0,80,0.5)'
+	},
+	4: {
+		backgroundColor: 'rgba(0,80,0,0.5)'
+	},
+	5: {
+		backgroundColor: 'rgba(80,0,0,0.5)'
+	},
+	6: {
+		backgroundColor: 'rgba(120,0,0,0.5)'
+	},
+	7: {
+		backgroundColor: 'rgba(160,0,0,0.5)'
+	},
+	8: {
+		backgroundColor: 'rgba(160,0,0,0.7)'
+	},
+	9: {
+		backgroundColor: 'rgba(200,0,0,0.7)'
+	},
+	10: {
+		backgroundColor: 'rgba(240,0,0,0.7)'
 	}
 };
